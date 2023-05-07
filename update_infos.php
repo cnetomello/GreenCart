ef0798b448888ab3955ade05ccdffa69fcc3f2e0
Cyril Zakhia <cyrilzakhia@hotmail.com>
2023-05-07 18:22:04 -0300

    Arrumando atualizacao immediata de dados na pagina usuario
    - Ao alterar informacoes, agora  os dados sao atualizados e exibidos como nas alteracoes feitas.
    - Coloquei um mensagem para avisar os usuario se os dados forem atualizado com sucesso ou nao.

--- update_infos.php
+++ update_infos.php
@@ -14,10 +14,20 @@ if($_SESSION['is_produtor']){
 
     $sql = "UPDATE infos_prod SET nome_empresa='$new_nome_empresa', email_prod='$new_email', fone_prod='$new_phone' WHERE id_prod='$prod_id'";
 
-    if($conn->query($sql) === TRUE){
-        echo "Dados atualizados com sucesso";
-    } else {
-        echo "Erro ao atualizar dados: " . $conn->error;
+    if($conn->query($sql) === TRUE){?>
+        <script>
+            alert( "Dados atualizados com sucesso");
+         </script>  <?php
+         $_SESSION['infos_pessoa_prod']['nome_empresa']=$new_nome_empresa ;
+         $_SESSION['infos_pessoa_prod']['email_prod']= $new_email ;
+         $_SESSION['infos_pessoa_prod']['fone_prod']= $new_phone ;
+         $_SESSION['update']=true;
+    } else {?>
+        <script>
+        alert( "Erro ao atualizar dados: " <?php echo $conn->error;?>);
+     </script>
+     <?php
+      $_SESSION['update']=false;
     }
     $conn->close();
     header('location: User.php');
@@ -34,12 +44,26 @@ else {
 
     $sql = "UPDATE infos SET first_name='$new_first_name', last_name='$new_last_name' , email='$new_email', phone='$new_phone' WHERE id='$comprador_id'";
 
-    if($conn->query($sql) === TRUE){
-        echo "Dados atualizados com sucesso";
-    } else {
-        echo "Erro ao atualizar dados: " . $conn->error;
+    if($conn->query($sql) === TRUE){ ?>
+    <script>
+        alert( "Dados atualizados com sucesso");
+     </script>  
+     <?php  
+        $_SESSION['infos_pessoa']['first_name'] =$new_first_name ;
+        $_SESSION['infos_pessoa']['last_name'] = $new_last_name ;
+        $_SESSION['infos_pessoa']['email'] = $new_email  ;
+        $_SESSION['infos_pessoa']['phone'] = $new_phone ;
+        $_SESSION['update']=true;
+    } else {?>
+        <script>
+           
+        alert( "Erro ao atualizar dados: " <?php echo $conn->error;?>);
+     </script>
+     <?php
+      $_SESSION['update']=false;
     }
     $conn->close();
+   
     header('location: User.php');
 
 
