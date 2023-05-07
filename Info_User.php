d770b90690b797a1a1ef991d412ff6013aa156d4
JulioDomanski <julio.domanski@hotmail.com>
2023-05-06 22:14:30 -0300

    info_user.php
    melhorando e finalizando a pagina info_user.php

--- Info_User.php
+++ Info_User.php
@@ -26,98 +26,88 @@ $phone =  isset($_SESSION['infos_pessoa']['phone']) ? $_SESSION['infos_pessoa'][
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
             <!-- custom css file link  -->
-            <link rel="stylesheet" href="CSS/index.css">
-            
-           
-
+            <link rel="stylesheet" href="CSS/profile.css">
         </head>
+        <header>
+
+            <input type="checkbox" name="" id="toggler">
+            <label for="toggler" class="fas fa-bars"></label>
+
+            <a href="index.php" class="logo">
+                <img src="images\greencart_carrinho-removebg-preview.png" width="300px;"></img>
+            </a>
+
+            <nav class="navbar">
+                <a href="index.php">Home</a>
+                <a href="index.php">Sobre</a>
+                <a href="produtos.php">Produtos</a>
+                <a href="avaliacoes.php">Avaliações</a>
+                <a href="index.php">Contato</a>
+            </nav>
+
+            <div class="icons">
+                <a href="#" class="fas fa-heart"></a>
+                <a href="carrinho.php" class="fas fa-shopping-cart"></a>
+                <a href="User.php" data-modal-target="#modal" class="fas fa-user"></a>
+            </div>
+
+        </header>
         <body>
-            
+        <section class="home" id="home">
+<?php
+if($_SESSION['is_produtor']){ ?>
 
+            <table>
+                <tr>
+                    <th>Nome Empresa:</th>
+                    <td><?php echo $nome_empresa?></td>
+                </tr>
+                <tr>
+                    <th>Email:</th>
+                    <td><?php echo $email_prod?></td>
+                </tr>
+                <tr>
+                    <th>Phone:</th>
+                    <td><?php echo $fone_prod?></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <button class="delete-btn" onclick="delete_acc()">Excluir Perfil</button>
+                        <button class="edit-btn" >Editar Perfil</button>
+                    </td>
+                </tr>
+            </table>
+        </body>
 
-            
-        <!-- header section starts  -->
+        <?php } else{ ?>
 
-            <header>
-
-                <input type="checkbox" name="" id="toggler">
-                <label for="toggler" class="fas fa-bars"></label>
-
-                <a href="index.php" class="logo">
-                    <img src="images\greencart_carrinho-removebg-preview.png" width="300px;"></img>
-                    </a>
-
-                <nav class="navbar">
-                    <a href="index.php">Home</a>
-                    <a href="index.php">Sobre</a>
-                    <a href="produtos.php">Produtos</a>
-                    <a href="avaliacoes.php">Avaliações</a>
-                    <a href="index.php">Contato</a>
-                </nav>
-
-                <div class="icons">
-                    <a href="#" class="fas fa-heart"></a>
-                    <a href="carrinho.php" class="fas fa-shopping-cart"></a>
-                    <a href="User.php" data-modal-target="#modal" class="fas fa-user"></a>
-                    
-                </div>
-               
-
-  </div>
-  
-            </header>
-            <section class="home" id="home">
-<?php if($_SESSION['is_produtor']){ ?>
-    <div style="display:block;">
-<div style="display: flex;">
-<div style="background-color: green;font-size: 20px;font-family:Verdana, Geneva, Tahoma, sans-serif; border-radius: 2px;align-items:center;color:white;"><h1>Nome Empresa : <?php echo $nome_empresa ?></h1></div> <button style="margin-left : 30px;font-size:15px; font-family:Verdana, Geneva, Tahoma, sans-serif;cursor:pointer;"><h1>Edit Nome Empresa</h1></button>
-
-</div> <br>
-
-
-
-
-<div style="display: flex;">
-<div style="background-color: green;font-size: 20px;font-family:Verdana, Geneva, Tahoma, sans-serif; border-radius: 2px;align-items:center;color:white;"><h1>Email : <?php echo $email_prod; ?></h1></div> <button style="margin-left : 30px;font-size:15px;font-family:Verdana, Geneva, Tahoma, sans-serif;cursor:pointer;"><h1>Edit Email</h1></button>
-
-</div> <br>
-
-<div style="display: flex;">
-<div style="background-color: green;font-size: 20px;font-family:Verdana, Geneva, Tahoma, sans-serif; border-radius: 2px;align-items:center;color:white;"><h1>Phone : <?php echo $fone_prod; ?></h1></div> <button style="margin-left : 30px;font-size:15px;font-family:Verdana, Geneva, Tahoma, sans-serif;cursor:pointer;"><h1>Edit Phone</h1></button>
-
-</div> <br>
-
-<button style="margin : 30px;font-size:15px;font-family:Verdana, Geneva, Tahoma, sans-serif; background-color: red;border-radius: 3px;cursor:pointer;" onclick="delete_acc();"><h1>Delete Account</h1></button>
-</div>
-
-
-
-
-    <?php } else{ ?>
-<div style="display:block;">
-<div style="display: flex;">
-<div style="background-color: green;font-size: 20px;font-family:Verdana, Geneva, Tahoma, sans-serif; border-radius: 2px;align-items:center;color:white;"><h1>First Name : <?php echo $first_name ?></h1></div> <button style="margin-left : 30px;font-size:15px; font-family:Verdana, Geneva, Tahoma, sans-serif; cursor:pointer;"><h1>Edit First Name</h1></button>
-
-</div> <br>
-
-<div style="display: flex;">
-<div style="background-color: green;font-size: 20px;font-family:Verdana, Geneva, Tahoma, sans-serif; border-radius: 2px;align-items:center;color:white;"><h1>Last Name : <?php echo $last_name ?></h1></div> <button style="margin-left : 30px;font-size:15px;font-family:Verdana, Geneva, Tahoma, sans-serif;cursor:pointer;"><h1>Edit Last Name</h1></button>
-
-</div> <br>
-<div style="display: flex;">
-<div style="background-color: green;font-size: 20px;font-family:Verdana, Geneva, Tahoma, sans-serif; border-radius: 2px;align-items:center;color:white;"><h1>Email : <?php echo $email; ?></div></h1> <button style="margin-left : 30px;font-size:15px;font-family:Verdana, Geneva, Tahoma, sans-serif;cursor:pointer;"><h1>Edit Email</h1></button>
-
-</div> <br>
-
-<div style="display: flex;">
-<div style="background-color: green;font-size: 20px;font-family:Verdana, Geneva, Tahoma, sans-serif; border-radius: 2px;align-items:center;color:white;"><h1>Phone : <?php echo $phone; ?></h1></div> <button style="margin-left : 30px;font-size:15px;font-family:Verdana, Geneva, Tahoma, sans-serif;cursor:pointer;"><h1>Edit Phone</h1></button>
-
-</div> <br>
-
-<button style="margin : 30px;font-size:15px;font-family:Verdana, Geneva, Tahoma, sans-serif; background-color: red;border-radius: 3px;cursor:pointer;" onclick="delete_acc();">Delete Account</button>
-</div>
-</body>
+            <table>
+                <tr>
+                    <th>First Name:</th>
+                    <td><?php echo $first_name?></td>
+                </tr>
+                <tr>
+                    <th>Last Name:</th>
+                    <td><?php echo $last_name?></td>
+                </tr>
+                <tr>
+                    <th>Phone:</th>
+                    <td><?php echo $phone?></td>
+                </tr>
+                <tr>
+                    <th>Email:</th>
+                    <td><?php echo $email?></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <button class="delete-btn" onclick="delete_acc()">Excluir Perfil</button>
+                        <button class="edit-btn" >Editar Perfil</button>
+                    </td>
+                </tr>
+            </table>
+        </body>
 <?php } ?>
+        
 <script>
     function delete_acc(){
         if(confirm('Are you sure you want to delete this account?')){
