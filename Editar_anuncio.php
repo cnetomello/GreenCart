c8a7483fa83ed15f855e730ea9fce03ab8652db5
JulioDomanski <julio.domanski@hotmail.com>
2023-05-12 18:50:10 -0300

    editar_anuncio.php
    exibindo os produtos anunciados no usuairo em uma tabela com o botao excluir e editar(q ainda n foram implementados)

--- Editar_anuncio.php
+++ Editar_anuncio.php
@@ -2,6 +2,8 @@
 session_start();
 if($_SESSION['is_produtor']){
     $first_name_prod =  isset($_SESSION['infos_pessoa_prod']['nome_empresa']) ? $_SESSION['infos_pessoa_prod']['nome_empresa'] : "" ;
+    $id_prod = isset($_SESSION['infos_pessoa_prod']['id']) ? $_SESSION['infos_pessoa_prod']['id'] : "" ;
+
 }
 ?>
 <!DOCTYPE html>
@@ -17,6 +19,55 @@ if($_SESSION['is_produtor']){
 
     <!-- custom css file link  -->
     <link rel="stylesheet" href="CSS/edit_anuncios.css">
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 300%;
+            max-width: 1000px;
+            margin: 0 auto;
+
+        }
+
+        th, td {
+            text-align: center;
+            padding: 8px;
+            border: 1px solid #ddd;
+        }
+
+        th {
+            background-color: #f2f2f2;
+            color: #333;
+            font-weight: bold;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        .btn-excluir, .btn-editar {
+            background-color: #f44336;
+            border: none;
+            color: white;
+            padding: 8px 16px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 10px;
+            margin: 4px 2px;
+            cursor: pointer;
+        }
+        .btn-editar {
+            background-color: #4CAF50;
+        }
+
+        .btn-excluir:hover {
+            background-color: #555;
+        }
+
+        .btn-editar:hover {
+            background-color: #555;
+        }
+    </style>
+
 
 
 </head>
@@ -55,6 +106,40 @@ if($_SESSION['is_produtor']){
 <section class="home" id="home">
     <!--aqui vai ficar a tabela q vai mostrar os anuncios do usuario produtor-->
     <div class="content">
+    <?php
+        include('connexion.php');
+        $sql = "SELECT * FROM anuncio_infos WHERE id_prod_an = '$id_prod'";
+        $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<thead><tr>
+                <th>Nome do produto</th>
+                <th>Descrição</th>
+                <th>Quantidade</th>
+                <th>Data da colheita</th>
+                <th>Preço unitário</th>
+                <th></th>
+                <th></th>
+          </tr></thead>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+
+            echo "<td>" . $row["nome_produto"] . "</td>";
+            echo "<td>" . $row["descricao"] . "</td>";
+            echo "<td>" . $row["qtd_produto"] . "</td>";
+            echo "<td>" . $row["data_colheta"] . "</td>";
+            echo "<td>" . $row["preco_unitario"] . "</td>";
+            echo "<td> <a href='#' class='btn-excluir'>excluir</a>  </td>";
+            echo "<td> <a href='#' class='btn-editar'>Editar</a>  </td>";
+
+            echo "</tr>";
+        }
+        echo "</table>";
+        } else {
+            echo "voce ainda n tem nenhum produto cadastrado.";
+        }
+    ?>
 
     </div>
 </section>
