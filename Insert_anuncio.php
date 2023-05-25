0ad003ad3454cc593ddba641a2ff2989044ceee8
Cyril Zakhia <cyrilzakhia@hotmail.com>
2023-05-25 12:14:11 -0300

    Imagens de Produtos
    Imagens dos produtos armazenadas no banco de dados exibidos com sucesso.

--- Insert_anuncio.php
+++ Insert_anuncio.php
@@ -9,9 +9,12 @@ $qtd_produto = $_POST['qtd_produto'];
 $desc_produto = $_POST['descricao_produto'];
 $preco_produto = $_POST['preco_produto'];
 $data_colheta = $_POST['data_colheta'];
-$foto_produto = $_POST['foto_produto'];
+$foto_nome = basename($_FILES['foto_produto']['name']);
+$foto_tipo = pathinfo($foto_nome, PATHINFO_EXTENSION);
+$image = $_FILES['foto_produto']['tmp_name']; 
+$imgContent = addslashes(file_get_contents($image)); 
 
-$sql = "INSERT INTO anuncio_infos(id_prod_an,nome_produto,qtd_produto,descricao,data_colheta,preco_unitario,foto_produto) values ('$id_prod','$nome_prod','$qtd_produto','$desc_produto','$data_colheta','$preco_produto','$foto_produto')";
+$sql = "INSERT INTO anuncio_infos(id_prod_an,nome_produto,qtd_produto,descricao,data_colheta,preco_unitario,foto_produto,tipo_foto) values ('$id_prod','$nome_prod','$qtd_produto','$desc_produto','$data_colheta','$preco_produto','$imgContent','$foto_tipo')";
 
  if ($conn->query($sql) === TRUE) {
      session_start();
